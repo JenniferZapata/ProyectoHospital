182ef998dfb6580cc886901403b9ff6f9ab12df9
SalgamosPues <jzz2310@gmail.com>
2024-06-10 15:42:36 -0500

    Se adicionan cambios del video 3

---
+++ src/components/herosection.jsx
@@ -0,0 +1,14 @@
+let HeroSection = ()=>{
+    return(
+        <>
+            <section id="hero" className="d-flex align-items-center">
+                <div className="container">
+                    <h1>Welcome to Medilab</h1>
+                    <h2>We are team of talented designers making websites with Bootstrap</h2>
+                    <a to="/login" className="btn-get-started scrollto">Get Started</a>
+                </div>
+            </section>
+        </>
+    )
+}
+export default HeroSection;
